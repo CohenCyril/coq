91f230651872e558007f539e51f7f36ca7913544
Quentin Vermande <quentin.vermande@orange.fr>
2025-09-08 16:45:41 +0200

    cleanup

--- pretyping/typing.ml
+++ pretyping/typing.ml
@@ -25,8 +25,6 @@ open Context.Rel.Declaration
 
 module GR = Names.GlobRef
 
-let debug_typing = CDebug.create ~name:"typing" ()
-
 let fresh_template_context env0 sigma ind (mib, _ as spec) ?(refresh_all=false) args =
   let templ = match mib.Declarations.mind_template with
   | None -> assert false
@@ -530,7 +528,6 @@ let check_binder_relevance env sigma s decl =
 (* cstr must be in n.f. w.r.t. evars and execute returns a judgement
    where both the term and type are in n.f. *)
 let rec execute env sigma cstr =
-  let () = debug_typing (fun () -> Pp.(v 0 (str "cstr: " ++ Termops.Internal.print_constr_env env sigma cstr ++ cut ()))) in
   let cstr = whd_evar sigma cstr in
   match EConstr.kind sigma cstr with
     | Meta n -> assert false (* Typing should always be performed on meta-free terms *)
